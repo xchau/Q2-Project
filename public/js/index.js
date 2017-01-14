68f3da8d8f0ca36dbff91ca6b610bf4416249ec8
Hiromi Heider <hiromiheider@gmail.com>
2017-01-14 15:28:23 -0800

    finished register/login mockup

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,21 @@
 (function() {
   'use strict';
 
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+
   $('.button-collapse').sideNav({
       menuWidth: 250,
       edge: 'right',
       closeOnClick: true
     }
   );
+
+  $('.submit').on('click', (event) => {
+    event.preventDefault();
+    alert("Hello Sarah")
+  });
 
 
 
