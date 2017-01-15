8eea1f031dc16f7afa8e8c68bac94b0b3c488f70
Hiromi Heider <hiromiheider@gmail.com>
2017-01-15 12:12:32 -0800

    made some migrations

---
+++ migrations/20170115113247_users.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('name').notNullable().defaultTo('');
+    table.string('email').unique().notNullable();
+    table.specificType('h_pw', 'char(60)').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
---
+++ migrations/20170115113310_fav_items.js
@@ -0,0 +1,25 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('fav_items', (table) => {
+    table.increments();
+    table.integer('item_id')
+      .notNullable()
+      .references('id')
+      .inTable('items')
+      .onDelete('CASCADE')
+      .index();
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .index();
+    table.timestamp('fav');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('fav_items');
+};
