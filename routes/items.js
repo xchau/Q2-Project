6f18a88516e1c0bf9d2f62eaf7665c82c164bce9
Minh Chau <xiu.chau@gmail.com>
2017-01-15 16:34:18 -0800

    Start on items.js route

--- routes/items.js
+++ routes/items.js
@@ -8,4 +8,10 @@ const knex = require('../knex');
 const { camelizeKeys, decamelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
-const router = express.Router(); 
+const router = express.Router();
+
+const authorize = function(req, res, next) {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    
+  })
+}
