f8b82fd5919c58753c023623cd21d63632d64ec1
Hiromi Heider <hiromiheider@gmail.com>
2017-01-16 23:17:49 -0800

    Close #38 Enable adding items in dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -37,4 +37,46 @@
       console.log($xhr.responseText);
       Materialize.toast($xhr.responseText, 3000);
     });
+
+  $('#add-item').submit((event) => {
+    event.preventDefault();
+
+    const title = $('#title').val().trim();
+    const itemDescription = $('#item-description').val().trim();
+    const imgFile = $('#img-file').val().trim();
+
+    if (!title) {
+      return Materialize.toast('Title must not be blank', 3000);
+    }
+
+    if (!itemDescription) {
+      return Materialize.toast('Item description must not be blank', 3000);
+    }
+
+    if (!imgFile) {
+      return Materialize.toast('Image file must not be blank', 3000);
+    }
+
+    const newItem = {
+      contentType: 'application/json',
+      data: JSON.stringify({
+        title,
+        description: itemDescription,
+        imagePath: imgFile
+      }),
+      dataType: 'json',
+      type: 'POST',
+      url: '/items'
+    };
+
+    $.ajax(newItem)
+      .done(() => {
+        console.log('testing');
+        window.location.href = '../dashboard.html'
+      })
+      .fail(($xhr) => {
+        console.log($xhr.responseText);
+        Materialize.toast($xhr.responseText, 3000);
+      });
+  });
 })();
